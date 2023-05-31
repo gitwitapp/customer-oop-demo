4a4374b753e5c31c6d626a12c65dae8aecfcf740
Gitwit <git@gitwit.dev>
2023-05-31 12:52:11 +0000

    Add class methods to Customer for serialization and deserialization

--- customer.py
+++ customer.py
@@ -4,6 +4,27 @@ class Customer:
         self.age = age
         self.email = email
         self.address = address
-        self.orders = []
-        self.payments = []
-        self.reviews = []
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "age": self.age,
+            "email": self.email,
+            "address": self.address,
+            "orders": self.orders,
+            "payments": self.payments,
+            "reviews": self.reviews
+        }
+
+    @classmethod
+    def deserialize(cls, data):
+        customer = cls(
+            name=data["name"],
+            age=data["age"],
+            email=data["email"],
+            address=data["address"]
+        )
+        customer.orders = data["orders"]
+        customer.payments = data["payments"]
+        customer.reviews = data["reviews"]
+        return customer
